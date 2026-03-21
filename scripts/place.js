60bf1a80c838f70aab12efcadb2d6ea9a2e1fc8d
shadygolden <shadrackchineduuzoka@gmail.com>
2026-03-21 05:07:33 +0100

    add edited place html

---
+++ scripts/place.js
@@ -0,0 +1,20 @@
+const temp = 8;
+const wind = 10;
+
+function calculateWindChill(t, v){
+return 13.12 + 0.6215*t - 11.37*Math.pow(v,0.16) + 0.3965*t*Math.pow(v,0.16);
+}
+
+let chill = "N/A";
+
+if(temp <= 10 && wind > 4.8){
+chill = calculateWindChill(temp, wind).toFixed(1) + "°C";
+}
+
+document.getElementById("chill").textContent = chill;
+
+// Footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+"Last Modified: " + document.lastModified;
