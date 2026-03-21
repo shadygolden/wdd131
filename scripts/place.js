90b37a8b10018cf84068974018bc1e9649557eb5
shadygolden <shadrackchineduuzoka@gmail.com>
2026-03-21 06:13:41 +0100

    add

--- scripts/place.js
+++ scripts/place.js
@@ -1,14 +1,14 @@
 const temp = 8;
 const wind = 10;
 
-function calculateWindChill(t, v){
-return 13.12 + 0.6215*t - 11.37*Math.pow(v,0.16) + 0.3965*t*Math.pow(v,0.16);
+function calculateWindChill(t, v) {
+    return 13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16);
 }
 
 let chill = "N/A";
 
-if(temp <= 10 && wind > 4.8){
-chill = calculateWindChill(temp, wind).toFixed(1) + "°C";
+if (temp <= 10 && wind > 4.8) {
+    chill = calculateWindChill(temp, wind).toFixed(1) + "°C";
 }
 
 document.getElementById("chill").textContent = chill;
@@ -17,4 +17,4 @@ document.getElementById("chill").textContent = chill;
 document.getElementById("year").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent =
-"Last Modified: " + document.lastModified;
+    "Last Modified: " + document.lastModified;
